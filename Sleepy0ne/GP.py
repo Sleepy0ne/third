67258c9362bec67d5427c81c9a14b4e4aec164fa
Sleepy0ne <112113369+Sleepy0ne@users.noreply.github.com>
2022-08-26 13:37:38 -0500

    Hp stat added and more lines in the game. The start of your adventure

--- Sleepy0ne/GP.py
+++ Sleepy0ne/GP.py
@@ -1,9 +1,23 @@
+def Player():
+  hp = 30
+  print(hp)
+ 
+ #player = 30
+ #def(enemy)
+  #enemy = 20
+  #def(sword)
+   #sword = -15   
 #The start
 name = input("Welcome, To begin. Please enter a Name. Let be known you will use this name all throught the game. ")
 yesorno = input(name + ", Are you sure this is the name you want? (Can't go back after you decide!)")
 if "yes" in yesorno:
   print("Then let's begin.")
+  print("You wake awaken deep in forest agaisnt a tree. It seems you've forgotten everything but you name. Your name is " + name + ". You look around a bit to notice large claw marks agaisnt the trees and dirt.")
+  choice = input("What do you do?")
+  if "Run away" in choice:
+    print(Player())
 else:
     name2 = input("Enter name: ")
     print(name2 + ", Hopefully this is what yu wanted. Let's begin." )
+    print("You wake awaken deep in forest agaisnt a tree. It seems you've forgotten everything but you name. Your name is " + name2 + ". You look around a bit to notice large claw marks agaisnt the trees and dirt.")
 
